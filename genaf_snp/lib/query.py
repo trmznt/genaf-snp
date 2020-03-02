40a89f1729d9a0abc2d223b3ac9299471937e1f0
Hidayat Trimarsanto <anto@eijkman.go.id>
2020-03-02 13:50:19 +0700

    add files for analysis & tools

---
+++ genaf_snp/lib/query.py
@@ -0,0 +1,6 @@
+
+from genaf_base.lib.query import Query as base_query
+
+class Query(base_query):
+	pass
+
---
+++ genaf_snp/views/tools/allele.py
@@ -0,0 +1,19 @@
+
+from genaf_snp.views.tools import ToolViewer
+
+
+class AlleleTool(ToolViewer):
+
+	def get_form(self):
+
+		form = super().get_form()
+
+		return form
+
+	def process(self):
+		pass
+
+	def format_result(self):
+		pass
+
+
