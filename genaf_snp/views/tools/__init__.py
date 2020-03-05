713d169dd518438cbcbdfeefc1bddb26ce4563cc
Hidayat Trimarsanto <anto@eijkman.go.id>
2020-03-05 08:50:04 +0700

    remove form fields intended for genaf_base

--- genaf_snp/views/tools/__init__.py
+++ genaf_snp/views/tools/__init__.py
@@ -15,63 +15,12 @@ class SNPAnalyticViewer(AnalyticViewer):
 
         d = super().parse_form(params)
 
-        # these needs to be in AnalysisViewer.parse_form()
-
-        d['batch_ids'] = params.get('genaf-query.batch_ids')
-        # XXX: we need to check batch_ids here
 
         return d
 
 
     def get_form(self, jscode="", params={}):
         qform, jscode = super().get_form(jscode, params)
-
-
-        # this should be in AnalysisViewer.get_form()
-
-        if self.request.user.has_roles( SYSADM, DATAADM, SYSVIEW, DATAVIEW ):
-            batches = self.dbh.get_batches( groups = None )
-        else:
-            batches = self.dbh.get_batches( groups = self.request.user.groups )
-
-        qform.get('genaf-query.sample-source').add(
-
-            input_select('genaf-query.batch_ids', 'Batch code(s)', offset=2, size=3,
-                value=params.get('genaf-query.batches', None),
-                options = [ (b.id, b.code) for b in batches ],
-                multiple=True,
-            ),
-
-            # 
-        )
-
-        qform.get('genaf-query.sample-processing').add(
-
-            input_select('genaf-query.sample_filtering', 'Sample filtering',
-                offset=2, size=3,
-                value='N',
-                options = [ ('N', 'No futher sample filtering'),
-                            ('M', 'Monoclonal samples'),
-                        ]
-                ),
-            input_select('genaf-query.spatial_differentiation', 'Spatial differentiation',
-                offset=2, size=3,
-                value=-1,
-                options = [ (-1, 'No spatial differentiation'),
-                            (0, 'Country level'),
-                            (1, '1st Administration level'),
-                            (2, '2nd Administration level'),
-                            (3, '3rd Administration level'),
-                            (4, '4th Administration level') ]
-                ),
-            input_select('genaf-query.temporal_differentiation', 'Temporal differentiation',
-                offset=2, size=3,
-                value=0,
-                options = [ (0, 'No temporal differentiation'),
-                            (1, 'Yearly'),
-                            (2, 'Quaterly')]
-                ),
-        )
 
         panels = self.dbh.get_panels()
 
@@ -91,6 +40,16 @@ class SNPAnalyticViewer(AnalyticViewer):
                                 ('MR', 'Majority calls (use ref allele for equal intensity/read ratio)'),
                 ],
                 multiple=False,
+            ),
+
+            input_text('genaf-query.allele_threshold', 'Intensity/read threshold',
+                offset=2, size=3,
+                value=5,
+            ),
+
+            input_text('genaf-query.SNP_cutoff', 'SNP quality cutoff',
+                offset=2, size=3,
+                value=0.5,
             ),
         )
 
