4e1b49dad762aaee2941439c896a6f40fc0f6c0f
Hidayat Trimarsanto <anto@eijkman.go.id>
2020-03-05 13:24:13 +0700

    add snp list view functionality

--- genaf_snp/models/dbschema.py
+++ genaf_snp/models/dbschema.py
@@ -25,73 +25,77 @@ class SNPSample(get_dbhandler_class().Sample, SampleMixIn):
 
 class Locus(BaseMixIn, Base, LocusMixIn):
 
-	__tablename__ = 'locuses'
+    __tablename__ = 'locuses'
 
-	code = Column(types.String(16), nullable=False, unique=True)
-	refseq = Column(types.String(16), nullable=False, server_default='')
-	position = Column(types.Integer, nullable=False, server_default='')
+    code = Column(types.String(16), nullable=False, unique=True)
+    refseq = Column(types.String(16), nullable=False, server_default='')
+    position = Column(types.Integer, nullable=False, server_default='')
 
-	# bases
-	ref = Column(types.String(1), nullable=False, server_default='N')
-	alt = Column(types.String(1), nullable=False, server_default='N')
-	alt2 = Column(types.String(1), nullable=False, server_default='N')
-	alt3 = Column(types.String(1), nullable=False, server_default='N')
+    # bases
+    ref = Column(types.String(1), nullable=False, server_default='N')
+    alt = Column(types.String(1), nullable=False, server_default='N')
+    alt2 = Column(types.String(1), nullable=False, server_default='N')
+    alt3 = Column(types.String(1), nullable=False, server_default='N')
 
-	flags = Column(types.Integer, nullable=False, server_default='0')
-	remark = deferred(Column(types.String(1024), nullable=False, server_default=''))
+    flags = Column(types.Integer, nullable=False, server_default='0')
+    remark = deferred(Column(types.String(1024), nullable=False, server_default=''))
 
-	__table_args__ = ( UniqueConstraint( 'refseq', 'position'), )
+    __table_args__ = ( UniqueConstraint( 'refseq', 'position'), )
 
-	@classmethod
-	def search(cls, code, session):
+    @classmethod
+    def search(cls, code, session):
 
-		q = cls.query(session).filter( func.lower(cls.code) == func.lower(code) )
-		if q.count() == 0: return None
-		return q.one()
+        q = cls.query(session).filter( func.lower(cls.code) == func.lower(code) )
+        if q.count() == 0: return None
+        return q.one()
 
 
 class Panel(BaseMixIn, Base, PanelMixIn):
 
-	__tablename__ = 'panels'
+    __tablename__ = 'panels'
 
-	code = Column(types.String(32), nullable=False, server_default='')
-	flags = Column(types.Integer, nullable=False, server_default='0')
-	remark = Column(types.String(1024), nullable=False, server_default='')
+    code = Column(types.String(32), nullable=False, server_default='')
+    flags = Column(types.Integer, nullable=False, server_default='0')
+    remark = Column(types.String(1024), nullable=False, server_default='')
 
 
 # Panel <-> Locus relationship
 locus_panel_table = Table('locus_panels', metadata,
-	Column('id', types.Integer, Sequence('locus_panel_sequence', optional=True),
-		primary_key=True),
-	Column('locus_id', types.Integer, ForeignKey('locuses.id'), nullable=False),
-	Column('panel_id', types.Integer, ForeignKey('panels.id'), nullable=False),
-	UniqueConstraint( 'locus_id', 'panel_id')
+    Column('id', types.Integer, Sequence('locus_panel_sequence', optional=True),
+        primary_key=True),
+    Column('locus_id', types.Integer, ForeignKey('locuses.id'), nullable=False),
+    Column('panel_id', types.Integer, ForeignKey('panels.id'), nullable=False),
+    UniqueConstraint( 'locus_id', 'panel_id')
 )
 
 
 class Genotype(BaseMixIn, Base, GenotypeMixIn):
 
-	__tablename__ = 'genotypes'
+    __tablename__ = 'genotypes'
 
-	locus_id = Column(types.Integer, ForeignKey('locuses.id', ondelete='CASCADE'),
-		nullable=False)
+    locus_id = Column(types.Integer, ForeignKey('locuses.id', ondelete='CASCADE'),
+        nullable=False)
+    locus = relationship(Locus, uselist=False)
 
-	sample_id = Column(types.Integer, ForeignKey('samples.id', ondelete='CASCADE'),
-		nullable=False)
+    sample_id = Column(types.Integer, ForeignKey('samples.id', ondelete='CASCADE'),
+        nullable=False)
+    sample = relationship(SNPSample, uselist=False,
+                backref=backref('genotypes', lazy='dynamic', passive_deletes=True))
+    """ link to sample """
 
-	# count for Amplicon, intensity * 1000 for sequenom
-	A = Column(types.Integer, nullable=False, server_default='0')
-	T = Column(types.Integer, nullable=False, server_default='0')
-	C = Column(types.Integer, nullable=False, server_default='0')
-	G = Column(types.Integer, nullable=False, server_default='0')
+    # count for Amplicon, intensity * 1000 for sequenom
+    A = Column(types.Integer, nullable=False, server_default='0')
+    T = Column(types.Integer, nullable=False, server_default='0')
+    C = Column(types.Integer, nullable=False, server_default='0')
+    G = Column(types.Integer, nullable=False, server_default='0')
 
-	value = Column(types.Integer, nullable=False, server_default='0.0')
-	raw_qual = Column(types.Float, nullable=False, server_default='0.0')
-	norm_qual = Column(types.Float, nullable=False, server_default='0.0')
+    value = Column(types.Integer, nullable=False, server_default='0.0')
+    raw_qual = Column(types.Float, nullable=False, server_default='0.0')
+    norm_qual = Column(types.Float, nullable=False, server_default='0.0')
 
-	# base type
-	call = Column(types.String(1), nullable=False, default='N')
+    # base type
+    call = Column(types.String(1), nullable=False, default='N')
 
-	flags = Column(types.Integer, nullable=False, server_default='0')
+    flags = Column(types.Integer, nullable=False, server_default='0')
 
-	__table_args__ = ( UniqueConstraint( 'locus_id', 'sample_id'), )
+    __table_args__ = ( UniqueConstraint( 'locus_id', 'sample_id'), )
--- genaf_snp/views/sample.py
+++ genaf_snp/views/sample.py
@@ -3,12 +3,33 @@ from genaf_snp.views import *
 
 class SNPSampleExtensionViewer(object):
 
-	def __init__(self, request, sample):
-		self.request = request
-		self.sample = sample
+    def __init__(self, request, sample):
+        self.request = request
+        self.sample = sample
 
 
-	def extend(self, html):
+    def extend(self, html):
 
-		# we extend the html
-		html.add( div('SNP list') )
+        # we extend the html
+        html.add( div('SNP list') )
+
+        table_body = tbody()
+
+        genotypes = sorted([ (g.locus.refseq, g.locus.position, g.call) for g in self.sample.genotypes ])
+        for (r,p,c) in genotypes:
+            table_body.add(
+                tr( td(r), td(p), td(c),
+                )
+            )
+
+        genotype_table = table(class_='table table-condensed table-striped')[
+            thead(
+                tr(
+                    th('Refseq'), th('Pos'), th('Allele'),
+                )
+            )
+        ]
+
+        genotype_table.add( table_body )
+
+        html.add(genotype_table)
