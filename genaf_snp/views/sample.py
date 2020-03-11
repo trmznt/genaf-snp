f1f07aebbb6bba67ceb26f938fc18556d39ae72e
Hidayat Trimarsanto <anto@eijkman.go.id>
2020-03-11 14:20:26 +0700

    change layout for genotype view

--- genaf_snp/views/sample.py
+++ genaf_snp/views/sample.py
@@ -15,17 +15,17 @@ class SNPSampleExtensionViewer(object):
 
         table_body = tbody()
 
-        genotypes = sorted([ (g.locus.refseq, g.locus.position, g.call) for g in self.sample.genotypes ])
-        for (r,p,c) in genotypes:
+        genotypes = sorted([ (g.locus.refseq, g.locus.position, g.A, g.C, g.G, g.T) for g in self.sample.genotypes ])
+        for (r,p,a,c,g,t) in genotypes:
             table_body.add(
-                tr( td(r), td(p), td(c),
+                tr( td(r), td(p), td(a), td(c), td(g), td(t),
                 )
             )
 
         genotype_table = table(class_='table table-condensed table-striped')[
             thead(
                 tr(
-                    th('Refseq'), th('Pos'), th('Allele'),
+                    th('Refseq'), th('Pos'), th('A'), th('C'), th('G'), th('T'),
                 )
             )
         ]
