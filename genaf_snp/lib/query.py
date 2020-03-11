aac47d714aa144de4796c91219da7093fb56752c
Hidayat Trimarsanto <anto@eijkman.go.id>
2020-03-11 14:09:58 +0700

    combine spatool and genaf_base Query classes

--- genaf_snp/lib/query.py
+++ genaf_snp/lib/query.py
@@ -1,6 +1,15 @@
 
-from genaf_base.lib.query import Query as base_query
+from rhombus.lib.utils import cerr, cout
+from genaf_base.lib import query as base_query
+from spatools.lib.analytics import query as spatool_query
+from spatools.lib.analytics import selector as spatool_selector
 
-class Query(base_query):
-	pass
+base_query.set_query_class( filter_class = spatool_selector.Filter)
 
+class Query(base_query.Query, spatool_query.Query):
+
+    def __init__(self, specs, dbhandler, **kwargs):
+        base_query.Query.__init__(self, specs, dbhandler, **kwargs)
+        # use self.specs which now contains spec class instances
+        spatool_query.Query.__init__(self, self.specs, dbhandler)
+        self.specs['filter'].marker_ids = dbhandler.get_locus_ids_from_panel_ids(self.specs['filter'].panel_ids)
